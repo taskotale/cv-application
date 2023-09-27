024fede32c197cad6a7053f8e838949ce559cf0e
taskotale <110062804+taskotale@users.noreply.github.com>
2023-09-27 17:06:09 -0400

    added color const to pdf for easy change

--- src/pdfGenerator.jsx
+++ src/pdfGenerator.jsx
@@ -7,6 +7,9 @@ import boldFont from './fonts/Poppins-Bold.ttf'
 import regularFont from './fonts/Poppins-regular.ttf'
 import italicFont from './fonts/Poppins-Italic.ttf'
 
+const accentColor = '#FDD71B'
+const colorOne = '#474545'
+const colorTwo = 'white'
 
 Font.register({
   family: 'PoppinsThin',
@@ -31,7 +34,7 @@ const PDFGenerator = ({handleShowPdf}) => {
   const YellowLine = () => {
     return (
       <Svg height="10" width="900">
-        <Line x1="0" y1="5" x2="380" y2="5" strokeWidth={2} stroke="#FDD71B" />
+        <Line x1="0" y1="5" x2="380" y2="5" strokeWidth={2} stroke={accentColor} />
       </Svg>)
     }
   const singleHighlight = (data) => {
@@ -47,7 +50,7 @@ const PDFGenerator = ({handleShowPdf}) => {
           {data.name.toUpperCase()}
         </Text>
         <Svg height="10" width="140">
-          <Line x1="0" y1="5" x2="280" y2="5" strokeWidth={2} stroke="#FDD71B" />
+          <Line x1="0" y1="5" x2="280" y2="5" strokeWidth={2} stroke={accentColor} />
         </Svg>
         <List/>
       </View>
@@ -129,13 +132,13 @@ const PDFGenerator = ({handleShowPdf}) => {
     icon:{
       width:'5mm', 
       height:'5mm', 
-      backgroundColor:'#FDD71B',
+      backgroundColor: accentColor,
       marginLeft:'0.55cm',
     },
     image: {
       width: '5cm',
       padding: '0.2cm',
-      backgroundColor:'white',
+      backgroundColor:colorTwo,
       marginTop:'1cm',
       marginLeft:'0.55cm',
       marginBottom:'1cm'
@@ -147,15 +150,15 @@ const PDFGenerator = ({handleShowPdf}) => {
       fontFamily:'PoppinsItalic'
     },
     leftContainerDisplay: {
-      backgroundColor:'#474545',
+      backgroundColor:colorOne,
       padding: '3px',
       flexDirection: 'column',
       width: '6.5cm',
-      color:'white',
+      color: colorTwo,
       fontSize: '4mm'
     },
     nameContainer:{
-      backgroundColor:'#FDD71B',
+      backgroundColor: accentColor,
       textAlign:'center',
       marginTop:'1.5cm',
       marginLeft:'0',
@@ -174,10 +177,10 @@ const PDFGenerator = ({handleShowPdf}) => {
       fontSize:'4mm'
     },
     rightContainerDisplay: {
-      backgroundColor: 'white',
+      backgroundColor: colorTwo,
       width:'14.5cm',
       marginRight:'0.55cm',
-      color:'#474545'
+      color: colorOne
     },
     rightContent:{
       marginLeft:'0.55cm',
@@ -203,7 +206,7 @@ const PDFGenerator = ({handleShowPdf}) => {
           {person.info.phone && <View style={styles.iconInfo}>
             <Svg style={styles.icon} viewBox='-100 -100 700 700' >
               <Path
-              fill='#000'
+              fill={colorOne}
               d='M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z'
               ></Path>
             </Svg>
@@ -212,7 +215,7 @@ const PDFGenerator = ({handleShowPdf}) => {
           {person.info.email && <View style={styles.iconInfo}>
             <Svg style={styles.icon} viewBox='-100 -100 700 700' >
               <Path
-              fill='#000'
+              fill={colorOne}
               d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"
               ></Path>
             </Svg>
@@ -221,7 +224,7 @@ const PDFGenerator = ({handleShowPdf}) => {
           {person.info.web && <View style={styles.iconInfo}>
             <Svg style={styles.icon} viewBox='-100 -100 700 700' >
               <Path
-              fill='#000'
+              fill={colorOne}
               d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"
               ></Path>
             </Svg>
@@ -230,7 +233,7 @@ const PDFGenerator = ({handleShowPdf}) => {
           {person.info.address && <View style={styles.iconInfo}>
             <Svg style={styles.icon} viewBox='-150 -100 700 700' >
               <Path
-              fill='#000'
+              fill={colorOne}
               d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"
               ></Path>
             </Svg>
